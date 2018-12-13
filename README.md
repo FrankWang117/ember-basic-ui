d7c50ae73a64d3a345ee73705e685abcb1b73650
Frank <wsgoodman@163.com>
2018-12-13 19:52:14 +0800

    组件-button 初始化。

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-ember-basic-ui
+# ember-basic-ui
+
+For [Pharbers](www.pharbers.com) projects.
+
+## Installation
+
+* `git clone <repository-url>` this repository
+* `cd ember-basic-ui`
+* `yarn install`
+
+## Running
+
+* `ember serve`
+* Visit your app at [http://localhost:4200](http://localhost:4200).
+
+## button  
+
+Default Setting: `block: false`,`disabled: false`,`icon: ''`,`reverse: false`,`type: 'default'`,`content:'default'`.  
+
+```handlerbars
+{{p-button
+    block=false,
+    content="default btn",
+    disabled=false,
+    icon='',
+    reverse=false,
+    type='default'
+    }}
+```
---
+++ addon/components/p-button.js
@@ -0,0 +1,112 @@
+import Component from '@ember/component';
+import layout from '../templates/components/p-button';
+
+export default Component.extend({
+	layout,
+	tagName: 'button',
+	content: 'default',
+	classNames: ['p-btn'],
+	classNameBindings: ['block:btn-block', 'type', 'reverse'],
+	attributeBindings: ['disabled',],
+
+	/**
+	 * Property to block level buttons
+	 * @property block
+	 * @type boolean
+	 * @default false
+	 * @public
+	 */
+	block: false,
+
+	/**
+	 * Property to disable the button
+	 * 
+	 * @property disabled
+	 * @type boolean
+	 * @default false
+	 * @public
+	 */
+	disabled: false,
+
+	/**
+	 * Property to show button icon
+	 * @property icon
+	 * @type string
+	 * @default ''
+	 * @public
+	 */
+	icon: '',
+
+	/**
+	 * When clicking the button this action is called with the value of the button (that is the value of the "value" property).
+	 *
+	 * @event onClick
+	 * @param {*} value
+	 * @public
+	 */
+	onClick: null,
+
+	/**
+	 * Property to reverse button style for the background color to dark
+	 * @property reverse
+	 * @type boolean
+	 * @default false
+	 * @public
+	 */
+	reverse: false,
+
+	/**
+	 * Property to show type of button
+	 * @property type
+	 * @type string
+	 * @default 'default'
+	 * @public
+	 */
+	type: 'default',
+	/**
+	 * @method click
+	 * @private
+	 */
+	click() {
+		let action = this.get('onClick');
+		// let preventConcurrency = this.get('preventConcurrency');
+
+		if (action === null) {
+			return;
+		}
+
+		// if (!preventConcurrency || !this.get('isPending')) {
+		//   let promise = (action)(this.get('value'));
+		//   if (promise && typeof promise.then === 'function' && !this.get('isDestroyed')) {
+		// 	this.set('state', 'pending');
+		// 	promise.then(() => {
+		// 	  if (!this.get('isDestroyed')) {
+		// 		this.set('state', 'fulfilled');
+		// 	  }
+		// 	}, () => {
+		// 	  if (!this.get('isDestroyed')) {
+		// 		this.set('state', 'rejected');
+		// 	  }
+		// 	}
+		// 	);
+		//   }
+		// }
+		if (true) {
+			let promise = (action)(this.get('value'));
+			if (promise && typeof promise.then === 'function' && !this.get('isDestroyed')) {
+				this.set('state', 'pending');
+				promise.then(() => {
+					if (!this.get('isDestroyed')) {
+						this.set('state', 'fulfilled');
+					}
+				}, () => {
+					if (!this.get('isDestroyed')) {
+						this.set('state', 'rejected');
+					}
+				}
+				);
+			}
+		}
+		return this.get('bubble');
+	},
+});
